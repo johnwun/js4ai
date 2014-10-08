28e9184a4b532a6db0c5813604cbf581dccd9f5c
Adam Green <wrokred@outlook.com>
2014-10-08 13:53:32 +0100

    Added HSB/V & HEX
    Added options for HSV/B colours, and true false for adding HEX at the end of the string.

--- renderSwatchLegend.jsx
+++ renderSwatchLegend.jsx
@@ -6,8 +6,9 @@
 //  You can configure spacing and value display by configuring the variables at the top
 //  of the script.
 //   update: v1.1 now tests color brightness and renders a white label if the color is dark.
-//   update: v1.2 uses adobe colour converter, rather than rgb colour conversion for a closer match.
-//   update: v1.3 adds multiple colour space values based on array printColors.
+//   update: v1.2 uses adobe colour converter, rather than rgb colour conversion for a closer match. - wrokred
+//   update: v1.3 adds multiple colour space values based on array printColors, and colour name separator - wrokred
+//   update: v1.4 add option for HEX web colour output, and HSB/V colours - wrokred
 //>=--------------------------------------
 // JS code (c) copyright: John Wundes ( john@wundes.com ) www.wundes.com
 // copyright full text here:  http://www.wundes.com/js4ai/copyright.txt
@@ -19,9 +20,10 @@ doc = activeDocument,
 swatches = doc.swatches,
 cols = 4, // number of columns in group
 displayAs = "RGBColor",  //or "CMYKColor"
-printColors = ["RGB", "CMYK", "LAB", "GrayScale"], // RGB, CMYK, LAB and/or GrayScale
+printColors = ["RGB", "CMYK", "LAB", "GrayScale", "HSB"], // RGB, CMYK, LAB, GrayScale and or HSB/HSV
 colorSeparator = " ", // Character used to separate the colours eg "|" output = R: XXX|G: XXX|B: XXX
-textSize = 10, // output text size value in points
+hex = true, // output RGB HEX values (RGB must be one of the print colours)
+textSize = 10, // Value in points
 rectRef=null,
 textRectRef=null,
 textRef=null,
@@ -68,12 +70,11 @@ for(var c=2,len=swatches.length;c<len;c++)
 
 function getColorValues(c)
 {
+    if (c.typename == "SpotColor") {
+        return getColorValues(c.spot.color);
+    };
     if(c.typename)
     {
-        if (c.typename == "SpotColor") {
-        return getColorValues(c.spot.color);
-        };
-        
         switch(c.typename)
         {
             case "RGBColor": sourceSpace = ImageColorSpace.RGB; colorComponents=[c.red,c.green,c.blue]; break;
@@ -81,12 +82,27 @@ function getColorValues(c)
             case "LabColor": sourceSpace = ImageColorSpace.LAB; colorComponents=[c.l,c.a,c.b]; break;
             case "GrayColor": sourceSpace = ImageColorSpace.GrayScale; colorComponents=[c.gray]; break;
         }
+        outputHex = new Array();
         outputColors = new Array();
         for (var i = printColors.length - 1; i >= 0; i--) {
 
-            targetSpace = ImageColorSpace[printColors[i]];
-            outputColors[i] = app.convertSampleColor(sourceSpace, colorComponents, targetSpace,ColorConvertPurpose.previewpurpose);
+            if (printColors[i] != "HSB" && printColors[i] != "HSV") {
+                targetSpace = ImageColorSpace[printColors[i]];
+                outputColors[i] = app.convertSampleColor(sourceSpace, colorComponents, targetSpace,ColorConvertPurpose.previewpurpose);
+            } else {
+                //convert to RGB
+                targetSpace = ImageColorSpace["RGB"];
+                convertionColors = app.convertSampleColor(sourceSpace, colorComponents, targetSpace,ColorConvertPurpose.previewpurpose);
+                // convert to HSV
+                outputColors[i] = rgb2hsv(convertionColors);
+            };
             for (var j = outputColors[i].length - 1; j >= 0; j--) {
+                if (hex == true && printColors[i] == "RGB") {
+                    outputHex[j] = Math.round(outputColors[i][j]).toString(16).toUpperCase();
+                    if (outputColors[i][j] <= 16) {
+                        outputHex[j] = "0" + outputHex[j];
+                    };
+                };
                 outputColors[i][j] = printColors[i].charAt(j)+": "+Math.round(outputColors[i][j]);
                 if (j == outputColors[i].length -1) {
                     outputColors[i][j] += "\r";
@@ -94,8 +110,12 @@ function getColorValues(c)
             };
             outputColors[i] = outputColors[i].join(colorSeparator);
 
+
         };
 
+        if (hex == true && outputHex[0]) {
+            outputColors[printColors.length] = "#"+outputHex.join("");
+        };
 
         return outputColors.join("");
 
@@ -104,20 +124,65 @@ function getColorValues(c)
 }
 
 function is_dark(c){
- if(c.typename)
- {
-    switch(c.typename)
-    {
-        case "CMYKColor":
-        return (c.black>50 || (c.cyan>50 &&  c.magenta>50)) ? true : false;
-        case "RGBColor":
-        return (c.red<100  && c.green<100 ) ? true : false;
-        case "GrayColor":
-        return c.gray > 50 ? true : false;
-        case "SpotColor":
+    if (c.typename == "SpotColor") {
         return is_dark(c.spot.color);
+    };
+    if(c.typename)
+    {
+        switch(c.typename)
+        {
+            case "RGBColor": sourceSpace = ImageColorSpace.RGB; colorComponents=[c.red,c.green,c.blue]; break;
+            case "CMYKColor": sourceSpace = ImageColorSpace.CMYK; colorComponents=[c.cyan,c.magenta,c.yellow,c.black]; break;
+            case "LabColor": sourceSpace = ImageColorSpace.LAB; colorComponents=[c.l,c.a,c.b]; break;
+            case "GrayColor": sourceSpace = ImageColorSpace.GrayScale; colorComponents=[c.gray]; break;
+        }
 
-        return false;
+        targetSpace = ImageColorSpace.GrayScale;
+        isDarkColors = app.convertSampleColor(sourceSpace, colorComponents, targetSpace,ColorConvertPurpose.previewpurpose);
+        lightness = Math.floor(isDarkColors[0]);
+
+        return lightness > 60 ? true : false;
     }
+    return false;
 }
-}
+
+function rgb2hsv (inputRGB) {
+    var hsvColors = new Array(3),
+    rr, gg, bb,
+    r = inputRGB[0] / 255,
+    g = inputRGB[1] / 255,
+    b = inputRGB[2] / 255,
+    h, s,
+    v = Math.max(r, g, b),
+    diff = v - Math.min(r, g, b),
+    diffc = function(c){
+        return (v - c) / 6 / diff + 1 / 2;
+    };
+
+    if (diff == 0) {
+        h = s = 0;
+    } else {
+        s = diff / v;
+        rr = diffc(r);
+        gg = diffc(g);
+        bb = diffc(b);
+
+        if (r === v) {
+            h = bb - gg;
+        }else if (g === v) {
+            h = (1 / 3) + rr - bb;
+        }else if (b === v) {
+            h = (2 / 3) + gg - rr;
+        }
+        if (h < 0) {
+            h += 1;
+        }else if (h > 1) {
+            h -= 1;
+        }
+    }
+    hsvColors[0] = (h * 360);
+    hsvColors[1] = (s * 100);
+    hsvColors[2] = (v * 100);
+
+    return hsvColors;
+};
